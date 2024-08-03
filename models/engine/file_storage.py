b056fb6060db85a1db61192c295c80e8f428e14a
Innocent <terminalkarabo@gmail.com>
2024-08-03 21:19:04 +0000

    class file storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,19 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        """Retrieve one object based on class and ID."""
+        if cls not in classes.values():
+            return None
+        key = "{}.{}".format(cls.__name__, id)
+        return self.__objects.get(key)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage matching the given class."""
+        if cls:
+            return len(self.all(cls))
+        return len(self.__objects)
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
